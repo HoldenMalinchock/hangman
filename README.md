53cd1cfe858075595cee23d77e0d6b465ed9c624
Holden Malinchock <h.malinchock@verizon.net>
2023-06-19 22:57:40 -0400

    Start of the project, cleaned out our templates

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # hangman
+
 A public hangman game using Deno Deploy with Deno KV
---
+++ routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div class="w-full h-screen flex flex-col items-center justify-between font-mono">
+      Stuff here
+    </div>
+  );
+}
